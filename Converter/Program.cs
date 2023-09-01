a866e9d9472394581aaea9887a3d29b0a34a8c9b
AtlasReunited <65644424+AtlasReunited@users.noreply.github.com>
2023-08-31 18:11:28 -0700

    Update Program.cs

--- Converter/Program.cs
+++ Converter/Program.cs
@@ -1,9 +1,9 @@
-﻿class Program
+class Program
 {
     static void Main(string[] args)
     {
-        string inputFile = @"C:\Users\thega\Downloads\IMG_5365.png";
-        string outputFolder = @"C:\Users\thega\Downloads\";
+        string inputFile = @"";
+        string outputFolder = @"";
 
 
         ImageConverterUtility.ConvertToJpg(inputFile, outputFolder);
