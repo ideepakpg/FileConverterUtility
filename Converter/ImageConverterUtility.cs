6a0faa367a53eb46f9df36e484509ff94331adcb
Atlas <thegalaxy2015@yahoo.com>
2023-08-31 17:59:41 -0700

    First Upload

---
+++ Converter/ImageConverterUtility.cs
@@ -0,0 +1,51 @@
+﻿using ImageMagick;
+using System.IO;
+
+public static class ImageConverterUtility
+{
+    public static void ConvertToPng(string inputFilePath, string outputFolderPath)
+    {
+        EnsureOutputDirectoryExists(outputFolderPath);
+
+        using (MagickImage image = new MagickImage(inputFilePath))
+        {
+            string pngFilePath = Path.Combine(outputFolderPath, Path.GetFileNameWithoutExtension(inputFilePath) + ".png");
+            image.Format = MagickFormat.Png;
+            image.Write(pngFilePath);
+        }
+    }
+    //To JPG
+    public static void ConvertToJpg(string inputFilePath, string outputFolderPath)
+    {
+        EnsureOutputDirectoryExists(outputFolderPath);
+
+        using (MagickImage image = new MagickImage(inputFilePath))
+        {
+            string jpgFilePath = Path.Combine(outputFolderPath, Path.GetFileNameWithoutExtension(inputFilePath) + ".jpg");
+            image.Format = MagickFormat.Jpg;
+            image.Quality = 90; // Set JPEG quality (0 to 100)
+            image.Write(jpgFilePath);
+        }
+    }
+    // Overload with quality parameter
+    public static void ConvertToJpg(string inputFilePath, string outputFolderPath, int quality)
+    {
+        EnsureOutputDirectoryExists(outputFolderPath);
+
+        using (MagickImage image = new MagickImage(inputFilePath))
+        {
+            string jpgFilePath = Path.Combine(outputFolderPath, Path.GetFileNameWithoutExtension(inputFilePath) + ".jpg");
+            image.Format = MagickFormat.Jpg;
+            image.Quality = quality; // Set JPEG quality (0 to 100)
+            image.Write(jpgFilePath);
+        }
+    }
+
+    private static void EnsureOutputDirectoryExists(string outputFolderPath)
+    {
+        if (!Directory.Exists(outputFolderPath))
+        {
+            Directory.CreateDirectory(outputFolderPath);
+        }
+    }
+}
---
+++ Converter/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string inputFile = @"C:\Users\thega\Downloads\IMG_5365.png";
+        string outputFolder = @"C:\Users\thega\Downloads\";
+
+
+        ImageConverterUtility.ConvertToJpg(inputFile, outputFolder);
+
+        Console.WriteLine("Conversion complete.");
+    }
+}
