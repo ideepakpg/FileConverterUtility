6d163edd6cd3e8764ab57a738b45926a7236fec3
Deepak PG <deepakpg121@gmail.com>
2023-09-19 21:10:05 +0530

    Revised user prompts for clarity and user-friendliness

--- Converter/FileConverterUtility.cs
+++ Converter/FileConverterUtility.cs
@@ -4,18 +4,18 @@ public abstract class FileConverterUtility
 {
     protected string? _inputFile;
     protected string? _outputFolder;
-    
-    
+
+
     protected void getLocations()
     {
-        Console.WriteLine("Paste the file path of your file:");
+        Console.WriteLine("Enter the path of the file you want to convert:");
         _inputFile = Console.ReadLine();
 
         StripQuotes(ref _inputFile);
-        
-        Console.WriteLine("Paste the output folder path (Blank for same folder):");
+
+        Console.WriteLine("Enter the path of the output folder (Leave empty for the same folder):");
         _outputFolder = Console.ReadLine();
-        
+
         if (string.IsNullOrWhiteSpace(_outputFolder))
         {
             _outputFolder = Path.GetDirectoryName(_inputFile);
@@ -40,18 +40,18 @@ public abstract class FileConverterUtility
         conversions[conversionType].Invoke();
         Console.WriteLine("Conversion complete.");
     }
-    
+
     protected static void StripQuotes(ref string? input)
     {
-        
+
 
         if (input != null && input.StartsWith("\"") && input.EndsWith("\""))
         {
             input = input.Substring(1, input.Length - 2);
         }
     }
-    
-    
+
+
     protected static void EnsureOutputDirectoryExists(string outputFolderPath)
     {
         if (!Directory.Exists(outputFolderPath))
